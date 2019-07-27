4791ffeb7270c695ecbb4eec176a1a6583bb89fd
diken <diken666@163.com>
2019-07-27 14:12:49 +0800

    改动

--- jobInfo.py
+++ jobInfo.py
@@ -1,7 +1,7 @@
 import def_fuc as fuc
 
 # 目标网站
-url = "https://www.zhipin.com/c101270100-p100999/s_302/?page=1&ka=page-1"
+url = "https://www.zhipin.com/c101270100-p100999/s_301/?page=1"
 # 模拟浏览器的user-agent
 UA = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
 headers = {
